b80465218678c0963389d45f174541a742a74797
The Social Robin <81664242+thesocialrobin@users.noreply.github.com>
2022-06-08 15:14:48 -0500

    Update masterpage.js

--- masterpage.js
+++ masterpage.js
@@ -7,6 +7,7 @@ import {session} from 'wix-storage';
 import {local} from 'wix-storage';
 
 $w.onReady(async function () {
+	HideElements();
 	//TODO: write your page related code here...
 	if(wixUsers.currentUser.loggedIn){
 		let user=(await wixData.query("users").eq("userId", wixUsers.currentUser.id).find()).items;
@@ -42,3 +43,13 @@ $w.onReady(async function () {
 
 
 });
+
+function HideElements () {
+ let url = wixLocation.url;
+ if (url === "https://www.unlockyourlifetoday.com/mp3s-in-english-dev/")
+    {
+        $w('#group38').hide();
+        } else {
+        $w('#group38').show();
+        }
+        }
